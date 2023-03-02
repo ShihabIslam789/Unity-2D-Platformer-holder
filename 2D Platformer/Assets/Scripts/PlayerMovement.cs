b52c3b7453257df921b8e385d91da73bd9460ec6
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-03-01 23:51:52 -0500

    Animation update
    Replaced placeholder movement classes from unity animation scenes

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,9 @@ public class PlayerMovement : MonoBehaviour
      [SerializeField]private float moveSpeed = 7f;
      [SerializeField]private float jumpForce = 14f;
 
+     private enum MovementState = {idle, running, jumping, falling}
+     
+
 
     // Start is called before the first frame update
     private void Start()
@@ -31,24 +34,38 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x,jumpForce);
         }
 
-       UpdateAnimationUpdate();
+       UpdateAnimationState();
     }
 
-    private void UpdateAnimationUpdate()
+    private void UpdateAnimationState()
     {
+        MovementState state;
+
          if (dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else 
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
         }
+
+        if (rb.velocity.y > .1f) 
+        {
+            state = MovementState.jumping;
+        }
+
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
 }
