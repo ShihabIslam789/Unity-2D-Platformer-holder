df8b6c8209fd7df16f4980169de505172296fbd7
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-03-02 19:52:08 -0500

    ground gravity
    added  restrictions to jump so plaer can be more grounded

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
      private Rigidbody2D rb;
+     private BoxCollider2D coll;
      private SpriteRenderer sprite;
      private Animator anim;
-     private float dirX = 0f;
+    [SerializeField] private LayerMask jumpableground;
+
+    private float dirX = 0f;
      [SerializeField]private float moveSpeed = 7f;
      [SerializeField]private float jumpForce = 14f;
 
-     private enum MovementState = {idle, running, jumping, falling}
+    private enum MovementState = {idle, running, jumping, falling};
      
 
 
@@ -19,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent < Rigidbody2D>();
+        coll = GetComponent <BoxCollider2D>();
         sprite = GetComponent <SpriteRenderer>();
         anim = GetComponent <Animator>();
     }
@@ -26,22 +30,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-         dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
 
-      if (Input.GetButtonDown("Jump"))
+      if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x,jumpForce);
         }
-
-       UpdateAnimationState();
+              UpdateAnimationState();
     }
 
     private void UpdateAnimationState()
     {
         MovementState state;
 
-         if (dirX > 0f)
+        if (dirX > 0f)
         {
             state = MovementState.running;
             sprite.flipX = false;
@@ -51,21 +54,24 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.running;
             sprite.flipX = true;
         }
-        else 
+        else
         {
             state = MovementState.idle;
         }
 
-        if (rb.velocity.y > .1f) 
+        if (rb.velocity.y > .1f)
         {
             state = MovementState.jumping;
         }
-
         else if (rb.velocity.y < -.1f)
         {
             state = MovementState.falling;
         }
-
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableground)
     }
 }
