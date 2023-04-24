cf91071c8cf560c28900e405f8a1a80617880cee
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-04-24 14:41:20 -0400

    Unity changes
    Added library additions to implement text prompts open in game

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-     private Rigidbody2D rb;
-     private BoxCollider2D coll;
-     private SpriteRenderer sprite;
-     private Animator anim;
-    [SerializeField] private LayerMask jumpableground;
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
-     [SerializeField]private float moveSpeed = 7f;
-     [SerializeField]private float jumpForce = 14f;
+    public float moveSpeed = 7f;
+    public float jumpForce = 14f;
 
-    private enum MovementState = {idle, running, jumping, falling};
-     
+    private enum MovementState { idle, running, jumping, falling }
 
+    [SerializeField] private AudioSource jumpSoundEffect;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb = GetComponent < Rigidbody2D>();
-        coll = GetComponent <BoxCollider2D>();
-        sprite = GetComponent <SpriteRenderer>();
-        anim = GetComponent <Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+         dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-      if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+            jumpSoundEffect.Play();
+            rb.velocity = new Vector2(rb.velocity.x,  jumpForce);
         }
-              UpdateAnimationState();
+
+        UpdateAnimationState();
     }
 
     private void UpdateAnimationState()
@@ -67,11 +70,12 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.falling;
         }
+
         anim.SetInteger("state", (int)state);
     }
 
     private bool IsGrounded()
     {
-       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableground)
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
